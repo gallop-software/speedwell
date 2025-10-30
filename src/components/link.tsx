037d86653736e258a453ad38a54ffe54cd8f6222
Chris Baldelomar <chris@webplant.media>
2025-10-29 23:28:59 -0500

    PICK: icon support

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -1,14 +1,46 @@
 import NextLink, { type LinkProps } from 'next/link'
 import { forwardRef } from 'react'
+import { Icon } from '@iconify/react'
+import { clsx } from 'clsx'
+
+type LinkWithIconProps = LinkProps &
+  React.ComponentPropsWithoutRef<'a'> & {
+    icon?: string
+    iconPlacement?: 'before' | 'after'
+    iconSize?: string
+  }
 
 export const Link = forwardRef(function Link(
-  props: LinkProps & React.ComponentPropsWithoutRef<'a'>,
+  {
+    icon,
+    iconPlacement = 'after',
+    iconSize = 'w-5 h-5',
+    children,
+    className,
+    ...props
+  }: LinkWithIconProps,
   ref: React.ForwardedRef<HTMLAnchorElement>
 ) {
+  const iconElement = icon ? (
+    <Icon
+      icon={icon}
+      className={clsx(
+        iconSize,
+        iconPlacement === 'before' && children ? 'mr-2' : '',
+        iconPlacement === 'after' && children ? 'ml-2' : ''
+      )}
+    />
+  ) : null
+
   return (
     <NextLink
       ref={ref}
+      className={className}
       {...props}
-    />
+    >
+      {iconPlacement === 'before' && iconElement}
+      {children}
+      {iconPlacement === 'after' && iconElement}
+    </NextLink>
   )
 })
