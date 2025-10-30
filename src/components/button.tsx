0e6b7bbd996ee2f969b7734dd2b607b24949db56
Chris Baldelomar <chris@webplant.media>
2025-10-29 21:43:33 -0500

    PICK: improved buttons

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,25 +5,50 @@ import { Icon } from '@iconify/react'
 import { Link } from './link'
 
 const variants = {
-  primary: clsx(
-    'inline-flex items-center justify-center',
-    'rounded-full border border-transparent bg-gray-950 shadow-md',
-    'text-white',
-    'disabled:bg-gray-950 disabled:opacity-40 hover:bg-gray-800'
-  ),
-  secondary: clsx(
-    'relative inline-flex items-center justify-center',
-    'rounded-full border border-transparent bg-white/15 shadow-md ring-1 ring-[#D15052]/15',
-    'after:absolute after:inset-0 after:rounded-full after:shadow-[inset_0_0_2px_1px_#ffffff4d]',
-    'text-gray-950',
-    'disabled:bg-white/15 disabled:opacity-40 hover:bg-white/20'
-  ),
-  outline: clsx(
-    'inline-flex items-center justify-center',
-    'rounded-full border border-transparent shadow-sm ring-2 ring-gray-950',
-    'text-gray-950',
-    'disabled:bg-transparent disabled:opacity-40 hover:bg-gray-950/5'
-  ),
+  primary: {
+    light: clsx(
+      'inline-flex items-center justify-center',
+      'rounded-full border border-transparent bg-gray-950 shadow-md',
+      'text-white',
+      'disabled:bg-gray-950 disabled:opacity-40 hover:bg-gray-800'
+    ),
+    dark: clsx(
+      'inline-flex items-center justify-center',
+      'rounded-full border border-transparent bg-white shadow-md',
+      'text-gray-950',
+      'disabled:bg-white disabled:opacity-40 hover:bg-gray-100'
+    ),
+  },
+  secondary: {
+    light: clsx(
+      'relative inline-flex items-center justify-center',
+      'rounded-full border border-transparent bg-white/15 shadow-md ring-1 ring-[#D15052]/15',
+      'after:absolute after:inset-0 after:rounded-full after:shadow-[inset_0_0_2px_1px_#ffffff4d]',
+      'text-gray-950',
+      'disabled:bg-white/15 disabled:opacity-40 hover:bg-white/20'
+    ),
+    dark: clsx(
+      'relative inline-flex items-center justify-center',
+      'rounded-full border border-transparent bg-gray-950/15 shadow-md ring-1 ring-white/15',
+      'after:absolute after:inset-0 after:rounded-full after:shadow-[inset_0_0_2px_1px_#00000040]',
+      'text-white',
+      'disabled:bg-gray-950/15 disabled:opacity-40 hover:bg-gray-950/20'
+    ),
+  },
+  outline: {
+    light: clsx(
+      'inline-flex items-center justify-center',
+      'rounded-full border border-transparent shadow-sm ring-2 ring-gray-950',
+      'text-gray-950',
+      'disabled:bg-transparent disabled:opacity-40 hover:bg-gray-950/5'
+    ),
+    dark: clsx(
+      'inline-flex items-center justify-center',
+      'rounded-full border border-transparent shadow-sm ring-2 ring-white',
+      'text-white',
+      'disabled:bg-transparent disabled:opacity-40 hover:bg-white/5'
+    ),
+  },
 }
 
 const sizes = {
@@ -45,6 +70,8 @@ type ButtonProps = {
   iconPlacement?: 'before' | 'after'
   iconSize?: string
   wrap?: boolean
+  dark?: boolean
+  target?: '_self' | '_blank'
 } & (
   | React.ComponentPropsWithoutRef<typeof Link>
   | (React.ComponentPropsWithoutRef<typeof HeadlessButton> & {
@@ -60,10 +87,13 @@ export function Button({
   iconPlacement = 'before',
   iconSize = 'w-5 h-5',
   wrap = false,
+  dark = false,
+  target = '_self',
   children,
   ...props
 }: ButtonProps) {
-  className = clsx(className, variants[variant], sizes[size])
+  const variantClass = dark ? variants[variant].dark : variants[variant].light
+  className = clsx(className, variantClass, sizes[size])
 
   const iconElement = icon ? (
     <Icon
@@ -95,6 +125,7 @@ export function Button({
     ) : (
       <Link
         {...props}
+        target={target}
         className={clsx('cursor-pointer', className)}
       >
         {content}
--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -4,14 +4,20 @@ import React from 'react'
 interface ButtonsProps {
   children: React.ReactNode
   className?: string
+  margin?: string
 }
 
-export function Buttons({ children, className }: ButtonsProps) {
+export function Buttons({
+  children,
+  className,
+  margin = 'mt-12 mb-8',
+}: ButtonsProps) {
   return (
     <div
       className={clsx(
         'content-wrapper',
-        'mt-12 flex flex-col gap-x-6 gap-y-4 sm:flex-row relative z-30 mb-8',
+        'flex flex-col gap-x-6 gap-y-4 sm:flex-row relative z-30',
+        margin,
         className
       )}
     >
