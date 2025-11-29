b05ff2c158d6b8d3e1ea95180c2f5fabf6c99427
Chris Baldelomar <chris@webplant.media>
2025-11-29 00:40:55 -0600

    update

---
+++ src/blocks/content-5.tsx
@@ -0,0 +1,40 @@
+import { Section, Heading, Paragraph } from '@/components'
+
+export default function Content5() {
+  return (
+    <Section
+      className="bg-body py-30"
+      innerAlign="content"
+    >
+      <Heading
+        as="h2"
+        id="services"
+        className="text-center"
+      >
+        Magna fugiat laboris ullamco
+      </Heading>
+      <Paragraph>
+        Incididunt mollit non veniam ipsum labore dolore ea dolor cillum officia
+        sit ex cupidatat et aliqua veniam excepteur labore nulla nisi magna
+        culpa laboris ad reprehenderit velit enim nisi et adipiscing occaecat
+        sunt dolor exercitation dolore ad eiusmod nostrud est reprehenderit
+        nostrud aliquip duis fugiat velit et aliqua laborum sunt qui nulla
+        consectetur
+      </Paragraph>
+      <Paragraph>
+        Anim ea aliquip qui irure magna exercitation fugiat irure velit nulla
+        enim lorem anim ad laborum eiusmod incididunt anim tempor elit culpa
+        deserunt tempor officia enim adipiscing amet anim esse ea occaecat
+        cupidatat ut nostrud proident proident culpa incididunt ea deserunt
+        laboris
+      </Paragraph>
+      <Paragraph>
+        Deserunt fugiat irure sit qui qui tempor tempor quis consequat aute
+        irure amet proident aliquip ea nulla duis aliqua consectetur consequat
+        commodo sunt do nisi ex anim id labore cillum dolor nostrud cupidatat
+        sint dolore pariatur laborum non reprehenderit laboris nulla voluptate
+        velit duis aliqua pariatur id consequat lorem excepteur
+      </Paragraph>
+    </Section>
+  )
+}
