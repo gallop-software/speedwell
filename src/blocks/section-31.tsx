7323dc58e9c46c46e05572cebeeace8df0987e50
Chris Baldelomar <chris@webplant.media>
2025-11-29 00:48:53 -0600

    update

---
+++ src/blocks/section-31.tsx
@@ -0,0 +1,105 @@
+import {
+  Section,
+  FancyHeading,
+  Swiper,
+  Testimonial1,
+  Quote,
+} from '@/components'
+
+export default function Section31() {
+  return (
+    <Section
+      imageSrc="/images/portfolio/kseniachernaya/pexels-kseniachernaya-3951746.jpg"
+      imageAlt="Dolore in elit"
+      overlayColor="bg-body/90"
+      className="py-30"
+    >
+      <FancyHeading
+        id="learn-more"
+        text="In labore"
+        accent="tempor incididunt labore"
+      />
+      <Swiper>
+        <Testimonial1
+          img="/images/profiles/pexels-nkhajotia-1516680.jpg"
+          cite="Veniam ipsum"
+        >
+          <Quote>
+            Laborum labore sed nisi veniam incididunt amet aute culpa sint
+            nostrud lorem nisi ea mollit sint ad non do labore laboris amet
+            eiusmod ea pariatur do officia ad anim laborum laboris aute
+            consequat consectetur veniam nulla ut nisi sint tempor velit
+            incididunt voluptate voluptate ad do laboris commodo nisi laborum
+            dolore consequat sint aute culpa cupidatat excepteur sit incididunt
+            cillum sit in aute ullamco nisi proident commodo incididunt lorem
+            aliquip in pariatur sint nostrud aliquip sint velit reprehenderit
+            dolore magna minim qui dolor duis commodo sed ea anim fugiat ad
+            aliquip nulla non nisi incididunt deserunt consequat et ipsum ad est
+            duis labore velit qui tempor et fugiat sint adipiscing lorem dolor
+          </Quote>
+        </Testimonial1>
+        <Testimonial1
+          img="/images/profiles/pexels-retamozo-gonzalo-454802313-26926485.jpg"
+          cite="Sunt nisi"
+        >
+          <Quote>
+            Et nulla duis nisi commodo proident ex deserunt enim sed duis lorem
+            nisi mollit ex nostrud est magna ullamco do mollit pariatur nulla
+            commodo ad sit eiusmod magna id ut officia mollit nostrud laborum
+            ipsum ullamco sit fugiat ullamco qui deserunt ullamco ad pariatur
+            mollit sunt aute magna aute exercitation deserunt culpa id aliquip
+            ut minim consequat velit ipsum mollit lorem commodo consequat
+            officia sunt enim velit veniam nisi aute id incididunt fugiat
+            commodo
+          </Quote>
+        </Testimonial1>
+        <Testimonial1
+          img="/images/profiles/pexels-skildring-12871464.jpg"
+          cite="Sit magna"
+        >
+          <Quote>
+            Eiusmod lorem ut laborum cupidatat quis amet aliquip laborum dolore
+            est dolor id minim occaecat dolore esse dolore dolore pariatur
+            laborum et sint excepteur veniam tempor incididunt commodo tempor
+            deserunt ut ut ex adipiscing cillum qui veniam incididunt
+            consectetur non tempor minim ex amet pariatur ut non tempor elit
+            quis incididunt amet adipiscing id excepteur dolore sint sint quis
+            deserunt nulla ipsum consequat reprehenderit labore esse qui
+            consequat do nulla sunt ut quis sed quis nulla aute non elit fugiat
+            occaecat non labore laboris sit excepteur duis ut laboris amet ex
+            occaecat nisi sunt adipiscing lorem enim aliquip cillum fugiat
+            consectetur deserunt anim labore cupidatat veniam labore dolor ipsum
+            cupidatat sed voluptate
+          </Quote>
+        </Testimonial1>
+        <Testimonial1
+          img="/images/profiles/pexels-anna-nekrashevich-6801642.jpg"
+          cite="Ea amet"
+        >
+          <Quote>
+            Est aliquip est ullamco laboris qui aliquip ad laborum voluptate
+            exercitation consequat ex qui ea anim minim dolor esse dolor ullamco
+            duis dolor anim quis adipiscing veniam officia adipiscing nisi sit
+            exercitation laborum enim culpa deserunt eiusmod dolor id pariatur
+            exercitation quis non ex sunt nisi dolor eiusmod velit eiusmod
+            excepteur sed laboris dolor dolore mollit laborum magna minim
+            consequat id consectetur proident elit occaecat mollit pariatur
+            magna aliquip duis magna sint ut ipsum duis ullamco ullamco nulla
+            commodo nisi quis est anim dolor sed magna ea cupidatat magna aute
+            sit reprehenderit id est excepteur sint dolore exercitation laboris
+            duis deserunt velit aute ullamco occaecat ex ut duis enim enim in
+            laboris laboris sint nulla enim velit et adipiscing nisi sunt sed
+            commodo ipsum dolore tempor elit ad veniam non lorem ut nostrud est
+            labore lorem occaecat et consequat sed ipsum aliquip magna officia
+            consectetur eiusmod mollit tempor sunt labore dolor ullamco velit
+            duis id velit ad sed velit anim laborum anim culpa lorem culpa elit
+            aliquip non culpa laboris lorem ex officia sit proident consectetur
+            ut velit cupidatat tempor dolor deserunt sunt id voluptate aute duis
+            commodo fugiat sunt eiusmod consectetur ea esse pariatur quis enim
+            cupidatat ex est duis occaecat nostrud irure
+          </Quote>
+        </Testimonial1>
+      </Swiper>
+    </Section>
+  )
+}
