36954d7b9190d117bf71344ec4b4e2d329de8868
Chris Baldelomar <chris@webplant.media>
2025-11-28 20:25:25 -0600

    added comment instructions

--- src/content/portfolio.tsx
+++ src/content/portfolio.tsx
@@ -25,57 +25,60 @@ import arrowRightIcon from '@iconify/icons-heroicons/arrow-right-20-solid'
 export default function Content() {
   return (
     <>
-      <Navbar />
-
-      <div className="relative">
-        <div className="absolute inset-y-0 left-0 w-full lg:w-3/4 bg-accent3 z-0"></div>
-        <div className="pt-0 pb-20 lg:py-24 px-6 mx-auto max-w-[1600px] relative">
-          <Columns reverseColumns>
-            <Column className="relative -mx-6">
-              <Image
-                className="w-full h-[450px] sm:h-[600px] lg:h-[800px] object-cover"
-                src="/images/portfolio/pexels-alex-qian-1180283-2343465.jpg"
-                alt="Cupidatat consequat anim adipiscing sint est culpa"
-                rounded="rounded-none"
-                size="large"
-                lazy={false}
-              />
-              <Accent className="absolute hidden lg:block -bottom-10 lg:bottom-auto left-6 lg:-top-20 lg:-left-40 transform -rotate-12">
-                dolor sit
-              </Accent>
-            </Column>
-            <Column className="mx-auto max-w-2xl lg:max-w-7xl">
-              <Heading
-                as="h1"
-                className="max-w-2xl"
-              >
-                Aliqua duis in culpa labore
-              </Heading>
-              <Paragraph>
-                Culpa dolor dolore nisi non consequat elit aliquip pariatur aute
-                cillum fugiat mollit occaecat aute sed sunt ut esse
-              </Paragraph>
-              <Paragraph>
-                Magna veniam magna velit exercitation fugiat sunt elit
-                reprehenderit velit exercitation deserunt sint aute fugiat ea
-                est commodo id lorem incididunt ea adipiscing qui ipsum officia
-                magna ullamco do sed lorem est occaecat ut ex consequat cillum
-                exercitation ex et
-              </Paragraph>
-              <Buttons className="">
-                <Button
-                  href="#services"
-                  icon={arrowDownIcon}
-                  iconPlacement="after"
+      {/* Hero 2 */}
+      <div>
+        <Navbar />
+        <div className="relative">
+          <div className="absolute inset-y-0 left-0 w-full lg:w-3/4 bg-accent3 z-0"></div>
+          <div className="pt-0 pb-20 lg:py-24 px-6 mx-auto max-w-[1600px] relative">
+            <Columns reverseColumns>
+              <Column className="relative -mx-6">
+                <Image
+                  className="w-full h-[450px] sm:h-[600px] lg:h-[800px] object-cover"
+                  src="/images/portfolio/pexels-alex-qian-1180283-2343465.jpg"
+                  alt="Cupidatat consequat anim adipiscing sint est culpa"
+                  rounded="rounded-none"
+                  size="large"
+                  lazy={false}
+                />
+                <Accent className="absolute hidden lg:block -bottom-10 lg:bottom-auto left-6 lg:-top-20 lg:-left-40 transform -rotate-12">
+                  dolor sit
+                </Accent>
+              </Column>
+              <Column className="mx-auto max-w-2xl lg:max-w-7xl">
+                <Heading
+                  as="h1"
+                  className="max-w-2xl"
                 >
-                  Ullamco ad irure incididunt est
-                </Button>
-              </Buttons>
-            </Column>
-          </Columns>
+                  Aliqua duis in culpa labore
+                </Heading>
+                <Paragraph>
+                  Culpa dolor dolore nisi non consequat elit aliquip pariatur
+                  aute cillum fugiat mollit occaecat aute sed sunt ut esse
+                </Paragraph>
+                <Paragraph>
+                  Magna veniam magna velit exercitation fugiat sunt elit
+                  reprehenderit velit exercitation deserunt sint aute fugiat ea
+                  est commodo id lorem incididunt ea adipiscing qui ipsum
+                  officia magna ullamco do sed lorem est occaecat ut ex
+                  consequat cillum exercitation ex et
+                </Paragraph>
+                <Buttons className="">
+                  <Button
+                    href="#services"
+                    icon={arrowDownIcon}
+                    iconPlacement="after"
+                  >
+                    Ullamco ad irure incididunt est
+                  </Button>
+                </Buttons>
+              </Column>
+            </Columns>
+          </div>
         </div>
       </div>
 
+      {/* Showcase 1 */}
       <Section className="bg-body py-30">
         <Columns>
           <Column>
@@ -174,6 +177,7 @@ export default function Content() {
         </Columns>
       </Section>
 
+      {/* Showcase 2 */}
       <Section className="py-30 bg-gradient-to-b from-body2 to-body-light">
         <Columns reverseColumns>
           <Column className="aspect-[4/5] relative">
@@ -251,6 +255,7 @@ export default function Content() {
         </Gallery>
       </Section>
 
+      {/* Showcase 3 */}
       <Section className="py-30 bg-body-dark">
         <Columns reverseColumns={false}>
           <Column className="aspect-auto relative">
@@ -323,6 +328,7 @@ export default function Content() {
         </Gallery>
       </Section>
 
+      {/* Showcase 4 */}
       <Section className="py-30 bg-body2">
         <Columns reverseColumns={true}>
           <Column className="aspect-auto relative">
@@ -397,6 +403,7 @@ export default function Content() {
         </Gallery>
       </Section>
 
+      {/* Showcase 5 */}
       <Section className="py-30 bg-body">
         <Columns reverseColumns={true}>
           <Column className="aspect-[6/8] relative">
@@ -492,6 +499,7 @@ export default function Content() {
         </Gallery>
       </Section>
 
+      {/* Section 4 */}
       <Section className="py-30 bg-body-light">
         <Columns reverseColumns>
           <Column className="aspect-[4/5] relative">
@@ -535,6 +543,7 @@ export default function Content() {
         </Columns>
       </Section>
 
+      {/* Cover 1 */}
       <Cover
         imageSrc="/images/portfolio/pexels-jworks1124-342800.jpg"
         imageAlt="Nulla est cillum"
@@ -549,6 +558,7 @@ export default function Content() {
         </Accent>
       </Cover>
 
+      {/* Testimonial 1 */}
       <Section className="bg-body2 py-20">
         <FancyHeading
           text="Dolore ex"
