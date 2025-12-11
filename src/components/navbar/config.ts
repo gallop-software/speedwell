c92b74af26c28d4fa476130b832c0fb3ffb06f92
Rabpreet <singhrabpreet38@gmail.com>
2025-12-11 15:00:04 -0600

    navbar for meet the team

--- src/components/navbar/config.ts
+++ src/components/navbar/config.ts
@@ -87,7 +87,7 @@ export const links: NavLink[] = [
         {
           name: 'Meet the Team',
           description: 'Get to know our talented designers',
-          href: '/commercial',
+          href: '/meet-the-team',
           icon: accountGroupIcon,
         },
         {
