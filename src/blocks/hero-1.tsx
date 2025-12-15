d37cc7beb88a5e3d6ca27b887cec9f26ca74e536
Chris Baldelomar <chris@webplant.media>
2025-12-15 00:15:48 -0600

    updated

--- src/blocks/hero-1.tsx
+++ src/blocks/hero-1.tsx
@@ -23,7 +23,7 @@ export default function Hero1() {
       <Gradient className="relative rounded-4xl ring-1 ring-black/5 ring-inset overflow-hidden m-2">
         <Navbar className="-mt-2" />
         <div className="mx-auto max-w-[1600px] relative">
-          <HeroCanvasBackground className="absolute top-0 left-0 w-[70%] lg:w-[40%] h-[524px] lg:h-full z-0 opacity-30" />
+          <HeroCanvasBackground className="absolute top-0 left-0 w-[90%] sm:w-[60%] lg:w-[40%] h-[524px] lg:h-full z-0 opacity-30" />
           <HeroCanvasBackground2 className="absolute -bottom-10 -right-10 w-[500px] h-[500px] z-0" />
           <Columns
             cols="grid-cols-1 lg:grid-cols-[3fr_2fr]"
