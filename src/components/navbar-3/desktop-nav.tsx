2d981657ed33089aa816a7e4fea3425a00ef25b0
Chris Baldelomar <chris@webplant.media>
2025-12-20 16:55:16 -0600

    udpated desktop nav 3

--- src/components/navbar-3/desktop-nav.tsx
+++ src/components/navbar-3/desktop-nav.tsx
@@ -49,7 +49,7 @@ export function DesktopNav({
                               : 'bg-gray-950 text-white hover:bg-gray-800'
                           )
                         : clsx(
-                            'flex h-full cursor-pointer items-center px-4 py-3 text-lg font-body font-medium bg-blend-multiply focus:outline-none rounded-lg',
+                            'flex h-full cursor-pointer items-center px-4 py-2 text-lg font-body font-medium bg-blend-multiply focus:outline-none rounded-lg',
                             dark
                               ? 'text-white hover:bg-white/10'
                               : 'text-contrast data-hover:bg-black/2.5'
@@ -138,7 +138,7 @@ export function DesktopNav({
                         : 'bg-gray-950 text-white hover:bg-gray-800'
                     )
                   : clsx(
-                      'flex items-center px-4 py-3 text-lg font-body font-medium bg-blend-multiply rounded-lg',
+                      'flex items-center px-4 py-2 text-lg font-body font-medium bg-blend-multiply rounded-lg',
                       dark
                         ? 'text-white hover:bg-white/10'
                         : 'text-contrast hover:bg-black/2.5'
