8e6898ab0a7685c6cc58af651bb1d879f97c38f1
Rabpreet <singhrabpreet38@gmail.com>
2026-01-08 13:15:46 -0600

    hero-20 update

--- src/blocks/hero-20.tsx
+++ src/blocks/hero-20.tsx
@@ -151,11 +151,6 @@ export default function Hero20() {
             </Button>
           </Buttons>
         </div>
-
-        {/* Scroll indicator */}
-        <div className="absolute bottom-10 left-1/2 -translate-x-1/2">
-          <div className="w-px h-16 bg-gradient-to-b from-transparent via-white/40 to-transparent"></div>
-        </div>
       </div>
     </div>
   )
