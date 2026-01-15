d7c4ea486bce56bc011835ccb600a5d72f84e84e
Chris Baldelomar <chris@webplant.media>
2026-01-15 15:26:59 -0600

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
     },
   },
   title: {
-    default: 'Speedwell | Lubbock Midwives & Birthing Center',
+    default: 'Speedwell | Next.js Website Template',
     template: '%s | Speedwell',
   },
   openGraph: {
