018b9aaa8340fc71b66cd679afb7cad0ffd4b6bc
Chris Baldelomar <chris@webplant.media>
2026-01-18 17:05:52 -0600

    Revert "testing fix for sticky navbar auto appear"
    This reverts commit 4b5835c3ed9951bc46bae473242529cf0e3bf3c5.

--- src/hooks/use-offset-top.tsx
+++ src/hooks/use-offset-top.tsx
@@ -1,26 +1,16 @@
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 import { state } from '@/state'
 
 // ----------------------------------------------------------------------
 
 export default function useOffsetTop(top?: number) {
   const isTop = top || 100
-  const isInitialMount = useRef(true)
 
   useEffect(() => {
     const onScroll = () => {
       state.lastScrollingDirection = state.scrollingDirection
       state.lastOffsetTop = state.offsetTop
       state.offsetTop = window.scrollY
-
-      // Skip direction update on initial mount to prevent HMR issues
-      if (isInitialMount.current) {
-        isInitialMount.current = false
-        // Just sync the offset values without changing direction
-        state.lastOffsetTop = state.offsetTop
-        return
-      }
-
       if (!state.lockScrollDirection) {
         if (state.offsetTop > state.lastOffsetTop) {
           state.scrollingDirection = 'down'
