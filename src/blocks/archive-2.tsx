0e7be54ce7602f18b48ba63e9e97b77a22f57d57
Chris Baldelomar <chris@webplant.media>
2026-01-23 18:07:38 -0600

    npm run blocks lite

--- src/blocks/archive-2.tsx
+++ src/blocks/archive-2.tsx
@@ -1,20 +1,10 @@
-import { Section, FancyHeading, Blog } from '@/components'
+import { ProBlock } from '@/components'
 
 export default function Archive2() {
   return (
-    <Section
-      id="showcase"
-      className="bg-body2 py-20"
-    >
-      <FancyHeading
-        text="Latest Projects"
-        accent="explore our portfolio"
-      />
-      <Blog
-        perPage={6}
-        categoriesInclude={['Portfolio']}
-        loadMoreText="See More of My Work"
-      />
-    </Section>
+    <ProBlock
+      blockSlug="archive-2"
+      blockName="Archive 2"
+    />
   )
 }
