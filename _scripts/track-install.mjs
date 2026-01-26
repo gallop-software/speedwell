71c93680d71d9f9301a47db772482d67273ff91f
Chris Baldelomar <chris@webplant.media>
2026-01-25 21:45:31 -0600

    update

--- _scripts/track-install.mjs
+++ _scripts/track-install.mjs
@@ -1,11 +1,15 @@
 import { readFile } from 'fs/promises'
 
+const controller = new AbortController()
+const timeout = setTimeout(() => controller.abort(), 5000)
+
 try {
   const pkg = JSON.parse(await readFile('./package.json', 'utf8'))
 
   await fetch('https://gallop.software/api/telemetry', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
+    signal: controller.signal,
     body: JSON.stringify({
       template: 'speedwell',
       version: pkg.version,
@@ -16,4 +20,6 @@ try {
   })
 } catch {
   // Silent fail - don't block installation
+} finally {
+  clearTimeout(timeout)
 }
