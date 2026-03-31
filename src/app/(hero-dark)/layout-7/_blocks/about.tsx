8a0ba9fa2011dd65614c330929aa64fedba91737
Chris Baldelomar <chris@webplant.media>
2026-03-30 19:56:33 -0500

    fixed color bug

--- src/app/(hero-dark)/layout-7/_blocks/about.tsx
+++ src/app/(hero-dark)/layout-7/_blocks/about.tsx
@@ -73,7 +73,7 @@ export default function About() {
           {/* Host info */}
           <div className="flex items-center gap-6">
             <div className="flex-shrink-0">
-              <Span className="w-16 h-16 rounded-full bg-gradient-to-br from-accent to-accent2 flex items-center justify-center text-body font-bold text-xl">
+              <Span color="text-accent-contrast" className="w-16 h-16 rounded-full bg-gradient-to-br from-accent to-accent2 flex items-center justify-center font-bold text-xl">
                 SC
               </Span>
             </div>
